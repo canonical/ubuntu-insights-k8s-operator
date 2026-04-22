726305c8b1a8a19f2b6bbc45a1898f617bcccef9
kkuo <kat.kuo@canonical.com>
2026-04-22 14:29:18 -0400

    Unpin postgresql for integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -83,7 +83,6 @@ def app(juju: jubilant.Juju, metadata: Dict[str, Any], charm_file: str, image: s
     juju.deploy(
         charm="postgresql-k8s",
         channel="14/stable",
-        revision=495,
         trust=True,
         config={"profile": "testing"},
     )
